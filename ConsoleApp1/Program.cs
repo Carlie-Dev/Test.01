252b94f6cba7d1535f1ecac6512d9e4941713290
Jeyson Peters <42493931+jeysonp@users.noreply.github.com>
2023-02-21 11:32:03 -0600

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection.Metadata;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a test program for me to try out GitHub. \nYou may enter your name bellow:");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Welcome," + userName + "\nWelcome to this pointless program! \nHave a cake.");
+            Console.WriteLine("  , , ,\n  | | | \n ~~~~~~~\n|       |\n ~~~~~~~\n ~~~~~~~\n|       |\n---------");
+        }
+    }
+}
