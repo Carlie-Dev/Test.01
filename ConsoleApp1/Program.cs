07608a93ce99602fe892dac66b57d100d4bcaa79
Jeyson Peters <ellepheonixgames@gmail.com>
2023-02-22 12:52:32 -0600

    Test_Attempt2
    This is some practice code

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,8 +6,11 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //Name input
             Console.WriteLine("This is a test program for me to try out GitHub. \nYou may enter your name bellow:");
             string userName = Console.ReadLine();
+            
+            //Name output and cake output
             Console.WriteLine("Welcome," + userName + "\nWelcome to this pointless program! \nHave a cake.");
             Console.WriteLine("  , , ,\n  | | | \n ~~~~~~~\n|       |\n ~~~~~~~\n ~~~~~~~\n|       |\n---------");
         }
